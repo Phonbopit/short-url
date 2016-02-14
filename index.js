8f24bd2a091672624e4f502aa15f1edbcb68ca06
Chai Phonbopit <phonbopit@gmail.com>
2016-02-14 18:39:43 +0700

    add index page

---
+++ index.js
@@ -0,0 +1,61 @@
+import Hapi from 'hapi';
+import chalk from 'chalk';
+import vision from 'vision';
+import handlebars from 'handlebars';
+
+const server = new Hapi.Server();
+
+server.connection({
+  host: 'localhost',
+  port: 5555
+});
+
+server.register(vision, err => {
+
+  server.views({
+    engines: {
+      html: handlebars
+    },
+    relativeTo: __dirname,
+    path: 'views'
+  })
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request, reply) => {
+
+    var url = encodeURIComponent(request.params.url);
+
+    reply.view('index');
+
+    //TODO
+
+    // 1. find a url in database
+    // 2. return original url.
+  }
+});
+
+server.route({
+  method: 'POST',
+  path: '/',
+  handler: (request, reply) => {
+
+    var url = request.payload.url;
+
+    //TODO
+
+    //1. save original to database.
+    //2. generate short url
+  }
+});
+
+server.start(err => {
+
+  if (err) {
+    throw err;
+  }
+
+  console.log(chalk.green(`Server is running at ${server.info.uri}`));
+})
